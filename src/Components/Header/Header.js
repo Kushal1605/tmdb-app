110d42f55089019dc34487d31e0a1696460ad769
Kushal1605 <kushalgupta1605@gmail.com>
2023-06-03 10:51:46 +0530

    Bug Fixed.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import './Header.css'
 const Header = () => {
-    return <span onClick={() => window.scroll(0, 0)} className='header'>Movie Recommendation App</span>
+    return <span onClick={() => window.scroll(0, 0)} className='header'>MovieFlix</span>
 };
 export default Header;
